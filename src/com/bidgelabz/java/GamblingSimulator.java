677386ac60b5aa160c000f85126dc66cf6ef363e
unknown <priyanka.rahate23@gmail.com>
2022-03-31 16:40:00 +0530

    LDay

--- src/com/bidgelabz/java/GamblingSimulator.java
+++ src/com/bidgelabz/java/GamblingSimulator.java
@@ -1,56 +1,64 @@
 package com.bidgelabz.java;
 import java.util.Random;
+import java.util.Scanner;
 public class GamblingSimulator {
-	public static final int STAKE_PER_DAY = 100;
-	public static final int BET_PER_GAME = 1;
-	public static final int WIN = 1;
-	public static int stake = 0;
-	static int daysWonCount = 0;
-	static int daysLostCount = 0;
-
-
-
-	public static void winCheck() {
-		Random random = new Random();
-		int betReturns = random.nextInt(2);
-		if (betReturns == WIN) {
-			stake++;
-		} else {
-			stake--;
-		}
-
-	}
-
-
-	public static void resignDayCheck() {
-		int totalStake = 0;
-		stake = 0;
-		while (stake != 50 && stake != -50) {
-			winCheck();
-		}
-		totalStake = stake + STAKE_PER_DAY;
-		System.out.println("Total stake for resign for a day is  " + totalStake);
-
-	}
-
-	public static void monthlyWinOrLossCheck() {
-		int day;
-		for (day = 1; day <= 20; day++) {
-			System.out.printf("day %d\n", day);
-			resignDayCheck();
-			if (stake == 50) {
-				daysWonCount++;
-			} else if (stake == -50) {
-				daysLostCount++;
-			}
-		}
-		System.out.printf("days won for a month = %d\ndays lost for a month = %d\n", daysWonCount, daysLostCount);
-
-	}
+	static int AMOUNT_OF_STAKE = 100;
+	static int TOTAL_AMOUNT_DIFF_IN_MONTH = 0;
+	static int BET_AMOUNT = 1;
+	static int WON = 1;
+	static int LOST = 0;
+	static int TOTAL_AMOUNT = 0;
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to Gambling Simulation");
-		monthlyWinOrLossCheck();
-	}
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Starting amount of stake is " + AMOUNT_OF_STAKE);
+		System.out.println("Amount to bet after every play " + BET_AMOUNT);
 
+		for (int i = 1; i <= 20; i++) {
+			int cashInHand = AMOUNT_OF_STAKE;
+			while (cashInHand > 50 && cashInHand < 150) {
+				int play = (int) Math.floor(Math.random() * 10) % 2;
+				System.out.println("Gamblers play:" + play);
+
+				switch (play) {
+				case 1:
+					cashInHand = cashInHand + BET_AMOUNT;
+					System.out.println("Total current cash gambler has " + cashInHand);
+					break;
+				case 0:
+					cashInHand = cashInHand - BET_AMOUNT;
+					System.out.println("Total current cash gambler has " + cashInHand);
+					break;
+				default:
+					System.out.println("default");
+				}
+
+			}
+
+			if (cashInHand == 150 || cashInHand == 50) {
+
+				System.out.println("Player would like to resign for current day" + i);
+			}
+			if (cashInHand > 100) {
+				int amountWonPerDay = cashInHand - AMOUNT_OF_STAKE;
+				System.out.println("Player won day" + i + " RS " + amountWonPerDay);
+
+			} else if (cashInHand < 100) {
+				int amountLostPerDay = AMOUNT_OF_STAKE - cashInHand;
+				System.out.println("Player lost day" + i + " RS " + amountLostPerDay);
+
+			}
+
+			TOTAL_AMOUNT = TOTAL_AMOUNT + cashInHand;
+		}
+
+		System.out.println("Total amount player has after 20 days as per day included = " + TOTAL_AMOUNT + " $");
+		TOTAL_AMOUNT_DIFF_IN_MONTH = TOTAL_AMOUNT - AMOUNT_OF_STAKE * 20;
+
+		if (TOTAL_AMOUNT_DIFF_IN_MONTH > 0) {
+			System.out.println("Total cash gambler won " + TOTAL_AMOUNT_DIFF_IN_MONTH + " $");
+		} else {
+			System.out.println("Total cash gambler lost in that month " + TOTAL_AMOUNT_DIFF_IN_MONTH + " $");
+		}
+	}
 }
