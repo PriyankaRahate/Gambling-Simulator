e49cb056647587d47eb0f3bd14f4cbf61c3b8ed4
unknown <priyanka.rahate23@gmail.com>
2022-03-31 13:57:30 +0530

    Gambling Program

---
+++ src/com/bidgelabz/java/GamblingSimulator.java
@@ -0,0 +1,18 @@
+package com.bidgelabz.java;
+
+public class GamblingSimulator {
+
+	public static void main(String[] args) {
+		int STAKE = 100;
+		int BET =  1;
+		System.out.println("Welcome To Gambling Simulator");
+		System.out.println("Gambler start with stake of $" + STAKE + " every day");
+		System.out.println("Gambler bet for $" + BET + " every game");
+	}
+
+
+
+
+	}
+
+
