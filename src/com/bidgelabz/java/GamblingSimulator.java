44afc5ba82e5e5c90d916ff29f36c9682f907b24
unknown <priyanka.rahate23@gmail.com>
2022-03-31 14:30:25 +0530

    Result 50%

--- src/com/bidgelabz/java/GamblingSimulator.java
+++ src/com/bidgelabz/java/GamblingSimulator.java
@@ -5,28 +5,37 @@ public class GamblingSimulator {
 	public static final int BET_PER_GAME = 1;
 	public static final int WIN = 1;
 	public static int stake = 0;
+
+
+
 	public static void winCheck() {
 		Random random = new Random();
 		int betReturns = random.nextInt(2);
 		if (betReturns == WIN) {
 			stake++;
-			System.out.println("win");
 		} else {
 			stake--;
-			System.out.println("loss");
 		}
 		System.out.println("stake = " + stake);
+
 	}
-		public static void main(String[] args) {
-			System.out.println("Welcome to Gambling Simulation");
+	public static void resignDayCheck() {
+		int totalStake = 0;
+		while (stake != 50 && stake != -50) {
 			winCheck();
-		
-
-	
+		}
+		totalStake = stake + STAKE_PER_DAY;
+		System.out.println("Total stake for resign for a day is  " + totalStake);
 
 	}
-		
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Gambling Simulation");
+		resignDayCheck();
 	}
+
+}
+
 		
 	
 		
